2e20e1b0f677f25274f359168936a80d18a33fd3
FillInjava <370915707@qq.com>
2016-09-21 20:52:49 +0800

    java8

--- userguide/src/test/java/com/lg/hibernate/guide/test/batch/BatchTest.java
+++ userguide/src/test/java/com/lg/hibernate/guide/test/batch/BatchTest.java
@@ -1,23 +1,27 @@
 package com.lg.hibernate.guide.test.batch;
 
 import com.lg.hibernate.guide.test.BaseEntityManagerFunctionalTestCase;
+
+import com.lg.hibernate.guide.test.BaseEntityManagerFunctionalTestCase;
 import com.lg.hibernate.userguide.model.Call;
 import com.lg.hibernate.userguide.model.Partner;
 import com.lg.hibernate.userguide.model.Person;
 import com.lg.hibernate.userguide.model.Phone;
-import org.hibernate.CacheMode;
-import org.hibernate.ScrollMode;
-import org.hibernate.ScrollableResults;
-import org.hibernate.Session;
+import org.hibernate.*;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.jboss.logging.Logger;
+import org.junit.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
+import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by liuguo on 2016/9/20.
  */
-public class BatchTest extends BaseEntityManagerFunctionalTestCase{
+public class BatchTest extends BaseEntityManagerFunctionalTestCase {
 
     private final static Logger log = Logger.getLogger(BatchTest.class);
 
@@ -31,6 +35,73 @@ public class BatchTest extends BaseEntityManagerFunctionalTestCase{
         };
     }
 
+    @Test
+    public void testScroll() {
+        withScroll();
+    }
+
+    @Test
+    public void testStatelessSession() {
+        withStatelessSession();
+    }
+
+    @Test
+    public void testBulk(){
+        doInJPA(this::entityManagerFactory,entityManager -> {
+            entityManager.persist(new Person("Vlad"));
+            entityManager.persist(new Person("Mihalcea"));
+        });
+
+        doInJPA(this::entityManagerFactory,entityManager -> {
+           entityManager.unwrap(Session.class).setJdbcBatchSize(10);
+        });
+
+        doInJPA(this::entityManagerFactory,entityManager -> {
+            String oldName = "Vlad";
+            String newName = "Alexandru";
+
+            int entities = entityManager
+                    .createQuery("update Person p set p.name=:newName where p.name=:oldName")
+                    .setParameter("newName",newName)
+                    .setParameter("oldName",oldName)
+                    .executeUpdate();
+
+            assertEquals(1,entities);
+        });
+
+        doInJPA(this::entityManagerFactory,entityManager -> {
+            String oldName = "Alexandru";
+            String newName = "Vlad";
+
+            int updatedEntities = entityManager
+                    .unwrap(Session.class)
+                    .createQuery("update Person p set p.name=:newName where p.name=:oldName")
+                    .setParameter("newName",newName)
+                    .setParameter("oldName",oldName)
+                    .executeUpdate();
+
+            assertEquals(1, updatedEntities);
+        });
+
+        doInJPA( this::entityManagerFactory, entityManager -> {
+            String oldName = "Vlad";
+            String newName = "Alexandru";
+
+            Session session = entityManager.unwrap( Session.class );
+            //tag::batch-bulk-hql-update-version-example[]
+            int updatedEntities = session.createQuery(
+                    "update versioned Person " +
+                            "set name = :newName " +
+                            "where name = :oldName" )
+                    .setParameter( "oldName", oldName )
+                    .setParameter( "newName", newName )
+                    .executeUpdate();
+            //end::batch-bulk-hql-update-version-example[]
+            assertEquals(1, updatedEntities);
+        } );
+
+
+    }
 
     private void withoutBatch(){
         EntityManager entityManager = null;
@@ -55,6 +126,7 @@ public class BatchTest extends BaseEntityManagerFunctionalTestCase{
            }
            throw e;
         } finally {
+
             if(entityManager != null){
                 entityManager.close();
             }
@@ -112,15 +184,84 @@ public class BatchTest extends BaseEntityManagerFunctionalTestCase{
 
             Session session = entityManager.unwrap(Session.class);
 
-            session.createQuery("select p from Person p")
+            scrollableResults =  session.createQuery("select p from Person p")
                     .setCacheMode(CacheMode.IGNORE)
                     .scroll(ScrollMode.FORWARD_ONLY);
+
+            int count = 0;
+            while (scrollableResults.next()){
+                Person p = (Person) scrollableResults.get(0);
+                processPerson(p);
+
+                if(++count % batchSize == 0){
+                    entityManager.flush();
+                    entityManager.clear();
+                }
+            }
+
+            txn.commit();
 
 
         } catch (Exception e) {
 
-            e.printStackTrace();
+            if(txn != null && txn.isActive()){
+                txn.rollback();
+            }
+            throw e;
         } finally {
+            if(scrollableResults != null){
+                scrollableResults.close();
+            }
+            if(entityManager != null){
+                entityManager.close();
+            }
         }
     }
+
+    private void withStatelessSession(){
+        withBatch();
+
+        SessionFactory sessionFactory = null;
+        StatelessSession statelessSession = null;
+        ScrollableResults results = null;
+
+        Transaction txn = null;
+        try {
+            sessionFactory = this.entityManagerFactory().unwrap(SessionFactory.class);
+            statelessSession = sessionFactory.openStatelessSession();
+
+            txn = statelessSession.getTransaction();
+
+            txn.begin();
+
+            results = statelessSession
+                    .createQuery("select p from Person p")
+                    .scroll(ScrollMode.FORWARD_ONLY);
+
+            while (results.next()){
+                Person p = (Person) results.get(0);
+                processPerson(p);
+                statelessSession.update(p);
+            }
+
+            txn.commit();
+        } catch (Exception e) {
+            if(txn != null && txn.getStatus() == TransactionStatus.ACTIVE) txn.rollback();
+            throw e;
+        } finally {
+            if(results != null){
+                results.close();
+            }
+            if(statelessSession != null){
+                statelessSession.close();
+            }
+        }
+    }
+    private void processPerson(Person Person) {
+        if ( Person.getId() % 1000 == 0 ) {
+            log.infof( "Processing [%s]", Person.getName());
+        }
+    }
+
+
 }
