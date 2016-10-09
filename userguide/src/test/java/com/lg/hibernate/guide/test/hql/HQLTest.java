0bf10aadc3a689706e1a9908bd5e93109eaefc1b
FillInjava <370915707@qq.com>
2016-10-09 18:23:57 +0800

    java8

--- userguide/src/test/java/com/lg/hibernate/guide/test/hql/HQLTest.java
+++ userguide/src/test/java/com/lg/hibernate/guide/test/hql/HQLTest.java
@@ -1,19 +1,20 @@
 package com.lg.hibernate.guide.test.hql;
 
 import com.lg.hibernate.guide.test.BaseEntityManagerFunctionalTestCase;
-import com.lg.hibernate.userguide.model.Call;
-import com.lg.hibernate.userguide.model.Person;
-import com.lg.hibernate.userguide.model.Phone;
-import org.junit.Assert;
+import com.lg.hibernate.userguide.model.*;
 import org.junit.Before;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
-
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -26,20 +27,75 @@ public class HQLTest extends BaseEntityManagerFunctionalTestCase{
         return new Class[]{
                 Person.class,
                 Phone.class,
-                Call.class
+                Call.class,
+                CreditCardPayment.class,
+                WireTransferPayment.class
         };
     }
 
     @Before
     public void init(){
         doInJPA(this::entityManagerFactory,entityManager -> {
-            entityManager.persist(new Person("lg"));
 
-            Person p1 = new Person();
-            Person p2 = new Person();
-
+            Person p1 = new Person("liuguo");
+            p1.setNickName("lg");
+            p1.setAddress("Earth");
+            //转成亚洲的时间
+            p1.setCreatedOn(Timestamp.from(LocalDateTime.of(2016,1,1,0,0,0).toInstant(ZoneOffset.of("+8"))));
+            p1.getAddresses().put(AddressType.HOME,"Home Address");
+            p1.getAddresses().put(AddressType.OFFICE,"Offoce Address");
             entityManager.persist(p1);
+
+            Person p2 = new Person("Mr.Jack");
+            p2.setAddress("Earth");
+            p2.setCreatedOn(Timestamp.from(LocalDateTime.of(2016,2,1,22,58,10).toInstant(ZoneOffset.of("+8"))));
             entityManager.persist(p2);
+
+            Person person3 = new Person("Dr_ John Doe" );
+            entityManager.persist(person3);
+
+            Phone phone1 = new Phone( "123-456-7890" );
+            phone1.setId( 1L );
+            phone1.setType( PhoneType.MOBILE );
+            phone1.getRepairTimestamps().add( Timestamp.from( LocalDateTime.of( 2016, 1, 1, 12, 0, 0 ).toInstant(ZoneOffset.of("+8")) ) );
+            phone1.getRepairTimestamps().add( Timestamp.from( LocalDateTime.of( 2016, 1, 1, 12, 0, 0 ).toInstant( ZoneOffset.of("+8") ) ) );
+            p1.addPhone(phone1);
+
+            Call call11 = new Call();
+            call11.setDuration( 12 );
+            call11.setTimestamp( Timestamp.from( LocalDateTime.of( 2016, 1, 1, 0, 0, 0 ).toInstant( ZoneOffset.UTC ) ) );
+
+            Call call12 = new Call();
+            call12.setDuration( 33 );
+            call12.setTimestamp( Timestamp.from( LocalDateTime.of( 2016, 1, 1, 1, 0, 0 ).toInstant( ZoneOffset.UTC ) ) );
+
+            phone1.addCall(call11);
+            phone1.addCall(call12);
+
+            Phone phone2 = new Phone( "098-765-4321" );
+            phone2.setId( 2L );
+            phone2.setType( PhoneType.LAND_LINE );
+
+            Phone phone3 = new Phone( "098-765-4320" );
+            phone3.setId( 3L );
+            phone3.setType( PhoneType.LAND_LINE );
+
+            p2.addPhone( phone2 );
+            p2.addPhone( phone3 );
+
+            CreditCardPayment creditCardPayment = new CreditCardPayment();
+            creditCardPayment.setCompleted( true );
+            creditCardPayment.setAmount( BigDecimal.ZERO );
+            creditCardPayment.setPerson( p1 );
+
+            WireTransferPayment wireTransferPayment = new WireTransferPayment();
+            wireTransferPayment.setCompleted( true );
+            wireTransferPayment.setAmount( BigDecimal.valueOf( 100 ) );
+            wireTransferPayment.setPerson( p2 );
+
+            entityManager.persist( creditCardPayment );
+            entityManager.persist( wireTransferPayment );
+
         });
     }
 
@@ -101,6 +157,42 @@ public class HQLTest extends BaseEntityManagerFunctionalTestCase{
             List list = query.getResultList();
 
             assertTrue(list.size() >0 );
+        });
+
+    }
+
+    @Test
+    public void testInnerJOIN() throws Exception {
+        doInJPA(this::entityManagerFactory,entityManager -> {
+            List<Person> persons = entityManager
+                    .createQuery(
+                            "select distinct pr " +
+                            "from Person pr " +
+                            "join pr.phones ph " +
+                            "where ph.type=:phoneType",Person.class)
+                    .setParameter("phoneType", PhoneType.MOBILE)
+                    .getResultList();
+        });
+
+    }
+
+    @Test
+    public void test_hql_collection_qualification_associations_1() {
+
+        doInJPA( this::entityManagerFactory, entityManager -> {
+            Long id = 1L;
+            //tag::hql-collection-qualification-example[]
+
+            // select all the calls (the map value) for a given Phone
+            List<Call> calls = entityManager.createQuery(
+                    "select ch " +
+                            "from Phone ph " +
+                            "join ph.callHistory ch " +
+                            "where ph.id = :id ", Call.class )
+                    .setParameter( "id", id )
+                    .getResultList();
+            //end::hql-collection-qualification-example[]
+            assertEquals(2, calls.size());
         });
 
     }
